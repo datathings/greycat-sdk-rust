1b71ef282c5512da8bff6b705fd09a8e7b6dfe62
Maxime Tricoire <maxime.tricoire@datathings.com>
2025-10-20 12:44:54 +0200

    ci build

--- examples/simple/build.rs
+++ examples/simple/build.rs
@@ -12,8 +12,19 @@ fn main() {
     //     .output()
     //     .expect("unable to run: greycat codegen rust");
 
-    if cfg!(target_os = "macos") {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    if target_os == "windows" {
+        // Only build staticlib for Windows
+        println!("cargo:rustc-cfg=crate_type=\"staticlib\"");
+    } else if target_os == "macos" {
+        // Quiet MacOS Clang for undefined symbols
         println!("cargo:rustc-link-arg=-Wl,-undefined");
         println!("cargo:rustc-link-arg=-Wl,dynamic_lookup");
+        // Build cdylib for MacOS
+        println!("cargo:rustc-cfg=crate_type=\"cdylib\"");
+    } else {
+        // Build cdylib for Unix-like systems
+        println!("cargo:rustc-cfg=crate_type=\"cdylib\"");
     }
 }
