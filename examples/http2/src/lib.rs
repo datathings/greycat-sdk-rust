89357c4eac8f84550940cbd54961f924eac368e4
Maxime Tricoire <maxime.tricoire@datathings.com>
2025-10-25 15:52:45 +0200

    clean

--- examples/http2/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// Use GreyCat allocator instead of default
-#[global_allocator]
-static GLOBAL: greycat::GreyCatAlloc = greycat::GreyCatAlloc;
-
-mod bindings;
-mod http2;
--- examples/simple/src/io2.rs
+++
@@ -1,6 +0,0 @@
-use greycat::prelude::*;
-
-#[greycat_fn]
-fn read_file(path: GcString) -> Result<String, std::io::Error> {
-    std::fs::read_to_string(path.as_str())
-}
