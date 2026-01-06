b7a74a875da2df0489f2a3eb8450b6d24a412451
Maxime Tricoire <maxime.tricoire@datathings.com>
2026-01-06 22:38:34 +0100

    7.5-stable

--- greycat-sys/src/lib.rs
+++ greycat-sys/src/lib.rs
@@ -2,6 +2,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-mod generated;
-pub use generated::*;
 mod ext;
+#[allow(unnecessary_transmutes)]
+#[allow(clippy::missing_safety_doc)]
+mod generated;
+
+pub use generated::*;
